5521f41ece59a32a51dcc465ba85f8a2c5d6095a
sgh317 <sm071@qq.com>
2017-11-02 09:51:54 +0800

    支付确认页，添加开户行所在地区

--- vienna/static/iyb/life_pay.jsx
+++ vienna/static/iyb/life_pay.jsx
@@ -32,10 +32,12 @@ class PayForm extends Form {
 		}
 		let v = [
 			{name:'开户银行', code:"bank", type:"select", req:"yes", value: bk, options:env.dict.bank, onChange: (r)=>{console.log(r);}},
-            {name:'银行帐号', code:"bankCard", type:"number", req:"yes", value: bc, mistake:"请输入正确的银行卡号", desc:"银行卡号码"},
-            {name:'开户行所在地区', code:"bankCity", type:"bankCity", value: bcity, company: env.company, refresh:"yes", req:"yes", desc:"开户行所在地"},
-			{name:'开户人', code:"bankUser", type:"static", req:"yes", value:env.order.detail.applicant.name},
+            {name:'银行帐号', code:"bankCard", type:"number", req:"yes", value: bc, mistake:"请输入正确的银行卡号", desc:"银行卡号码"}
 		];
+	    if(env.company == "shlife"){
+            v.push({name:'开户行所在地区', code:"bankCity", type:"bankCity", value: bcity, company: env.company, refresh:"yes", req:"yes", desc:"开户行所在地"});
+		}
+	    v.push({name:'开户人', code:"bankUser", type:"static", req:"yes", value:env.order.detail.applicant.name});
 		return this.buildForm(v);
 	}
 }
