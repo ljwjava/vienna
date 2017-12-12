a0940156e87e42d761261783bf1803c8a225f475
sgh317 <sm071@qq.com>
2017-12-12 18:37:26 +0800

    selector修改

--- vienna/static/common/widget.selecter.jsx
+++ vienna/static/common/widget.selecter.jsx
@@ -3,8 +3,18 @@
 import React from 'react';
 
 var Selecter = React.createClass({
+    getInitialState() {
+        return {/*showAddit: true*/};
+    },
 	val() {
 		return this.refs.self.value;
+	},
+	addit(){
+        let code = this.val();
+        for (var v in this.props.options)
+            if (this.props.options[v].length > 2 && this.props.options[v][0] == code)
+                return this.props.options[v][2];
+        return null;
 	},
 	text() {
 		let code = this.val();
@@ -30,7 +40,10 @@ var Selecter = React.createClass({
 	render() {
 		if (this.props.options == null || this.props.options.length == 0)
 			return (<span ref="self"></span>);
-		let btns = this.props.options.map(v => (<option key={v[0]} value={v[0]}>{v[1]+(v.length > 2 ? ">"+v[2] : "")}</option>));
+		let showAddit = true;
+		if (this.props.showAddit != null)
+            showAddit = this.props.showAddit;
+		let btns = this.props.options.map(v => (<option key={v[0]} value={v[0]}>{v[1]+((v.length > 2 && showAddit) ? ">"+v[2] : "")}</option>));
 		return (<select ref="self" onChange={this.onChange} defaultValue={this.props.value}>{btns}</select>);
 	}
 });
