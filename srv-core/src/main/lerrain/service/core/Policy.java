2903e8ddec2d98841b786594049d6516b95c4660
lex1982103 <lerrain@gmail.com>
2018-02-01 18:43:11 +0800

    New changelist

---
+++ srv-core/src/main/lerrain/service/core/Policy.java
@@ -0,0 +1,30 @@
+package lerrain.service.core;
+
+import lerrain.tool.Common;
+
+import java.util.Map;
+
+public class Policy
+{
+    Map<String, Object> val;
+
+    public Policy(Map<String, Object> val)
+    {
+        this.val = val;
+    }
+
+    public Object get(String key)
+    {
+        return val.get(key);
+    }
+
+    public String getString(String key)
+    {
+        return Common.trimStringOf(get(key));
+    }
+
+    public Long getLong(String key)
+    {
+        return Common.toLong(get(key));
+    }
+}
