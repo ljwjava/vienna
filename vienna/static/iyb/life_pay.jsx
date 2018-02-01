348a68bba9849a87edd2dfe5d0760252bbc0f88f
sgh317 <sm071@qq.com>
2018-02-01 16:22:19 +0800

    分离callback/*.do

--- vienna/static/iyb/life_pay.jsx
+++ vienna/static/iyb/life_pay.jsx
@@ -52,13 +52,14 @@ var Ground = React.createClass({
 	},
 	componentWillMount() {
         common.req("dict/view.json", {company: env.company, name: "bank,relation", version: "new"}, r => {
-        	// console.log(Array.isArray(r.bank));
         	// if (typeof r.bank === 'object' && isNaN(r.bank.length)) {
-        	if (r.bank != null && !Array.isArray(r.bank)) {
-                env.dict.bank = r.bank[env.order.detail.applicant.city.code.substr(0, 2)];
-			} else {
-                env.dict.bank = r.bank;
-            }
+			if(r.bank != null){
+                if (!Array.isArray(r.bank)) {
+                    env.dict.bank = r.bank[env.order.detail.applicant.city.code.substr(0, 2)];
+                } else {
+                    env.dict.bank = r.bank;
+                }
+			}
             for (var l in r.relation) {
                 env.dict.relation[l.code] = l.text;
             }
