a88d844c4938013205b442e1793595abfd1e6e5a
lex1982103 <lerrain@gmail.com>
2018-03-06 11:09:24 +0800

    New changelist

---
+++ vienna/static/policy/life_list.jsx
@@ -0,0 +1,127 @@
+"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import List from '../common/component.list.jsx';
+
+var env = {
+    company: {},
+    bizType: {},
+    insType: {},
+    search: null,
+    from: 0,
+    number: 20
+}
+
+class PolicyList extends List {
+    open(id) {
+        document.location.href = "policy.web?policyId=" + id;
+    }
+    componentDidMount() {
+        super.componentDidMount();
+        common.req("btbx/channel/company.json", {}, r => {
+            if (r != null) env.company = r;
+            this.setState({});
+        });
+    }
+    refresh() {
+        common.req("btbx/policy/list.json", env, r => {
+            this.setState({content:r});
+        });
+    }
+    buildTableTitle() {
+        return (
+			<tr>
+                <th><div>保险公司</div></th>
+                <th><div>主要产品</div></th>
+                <th><div>保单号</div></th>
+				<th><div>投保人</div></th>
+                <th><div>被保险人</div></th>
+				<th><div>投保时间</div></th>
+				<th><div>保费</div></th>
+                <th><div>业务员</div></th>
+				<th>操作</th>
+			</tr>
+        );
+    }
+    buildTableLine(v) {
+        return (
+			<tr key={v.id}>
+                <td>{env.company[v.companyId].name}</td>
+                <td>{v.productName}</td>
+                <td>{v.policyNo}</td>
+				<td>{v.applicantName}</td>
+                <td>{v.insurantName}</td>
+				<td>{v.insureTime}</td>
+				<td style={{textAlign:"right"}}>{v.premium}</td>
+                <td>{v.owner}</td>
+				<td>
+                    <div className="btn-group" role="group">
+                        <button type="button" className="btn btn-default" onClick={this.open.bind(this, v.id)}>编辑</button>
+                        <button type="button" className="btn btn-danger">删除</button>
+                    </div>
+                </td>
+			</tr>
+        );
+    }
+}
+
+var Main = React.createClass({
+    render() {
+        return <div className="form-horizontal">
+			<div className="form-group">
+				<div className="col-sm-12">
+					<div className="container-fluid">
+						<ul className="nav navbar-nav">
+						</ul>
+						<ul className="nav navbar-nav navbar-right">
+							<li id="upload"><a>拖拽至此处上传</a></li>
+						</ul>
+					</div>
+					<PolicyList env={env}/>
+				</div>
+			</div>
+		</div>;
+    }
+});
+
+$(document).ready( function() {
+    ReactDOM.render(<Main/>, document.getElementById("content"));
+
+    $(document).on({
+        dragleave:function(e){    //拖离
+            e.preventDefault();
+        },
+        drop:function(e){  //拖后放
+            e.preventDefault();
+        },
+        dragenter:function(e){    //拖进
+            e.preventDefault();
+        },
+        dragover:function(e){    //拖来拖去
+            e.preventDefault();
+        }
+    });
+
+    var box = document.getElementById('upload'); //拖拽区域
+    box.addEventListener("drop", function(e){
+        e.preventDefault();
+        var fileList = e.dataTransfer.files;
+        if(fileList.length == 0)
+            return false;
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("post", common.url("btbx/policy/upload.file"), true);
+        xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+        xhr.onreadystatechange = function() {};
+
+        var fd = new FormData();
+        fd.append("index", env.index);
+        fd.append("path", env.dir);
+        for (var i=0;i<fileList.length;i++)
+            fd.append("file", fileList[i]);
+        xhr.send(fd);
+
+        console.log(xhr.response);
+    }, false);
+});
