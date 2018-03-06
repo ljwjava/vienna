a2711a72da23bc58c151fdbe479e17a7083805f2
sgh317 <sm071@qq.com>
2018-03-06 20:30:44 +0800

    中信保诚祯爱优选定寿界面调整

--- vienna/static/iyb/life_pay.jsx
+++ vienna/static/iyb/life_pay.jsx
@@ -25,30 +25,108 @@ env.dict = {
 };
 
 class PayForm extends Form {
-	form() {
-	    let bc = null;
-	    let bk = null;
-	    let bcity = null;
-	    if (env.order.extra != null && env.order.extra.pay != null) {
+    form() {
+        let bc = null;
+        let bk = null;
+        let bcity = null;
+        let bauto = null;
+        if (env.order.extra != null && env.order.extra.pay != null) {
             bc = env.order.extra.pay.bankCard;
             bk = env.order.extra.pay.bank;
             bcity = env.order.extra.pay.bankCity;
-		}
-		let v = [
-			{name:'开户银行', code:"bank", type:"select", req:"yes", value: bk, options:env.dict.bank},
+            bauto = env.order.extra.pay.autoPayFlag;
+        }
+        let v = [
+            {name:'开户银行', code:"bank", type:"select", req:"yes", value: bk, options:env.dict.bank},
             {name:'银行帐号', code:"bankCard", type:"number", req:"yes", value: bc, mistake:"请输入正确的银行卡号", desc:"银行卡号码"}
-		];
-	    if(env.company == "shlife") {
+        ];
+        if(env.company == "shlife" || env.company == "citicpru") {
             v.push({name:'开户行所在地区', code:"bankCity", type:"bankCity", value: bcity, company: env.company, refresh:"yes", req:"yes", desc:"开户行所在地"});
-		}
-	    v.push({name:'开户人', code:"bankUser", type:"static", req:"yes", value:env.order.detail.applicant.name});
-		return this.buildForm(v);
-	}
+        }
+        v.push({name:'开户人', code:"bankUser", type:"static", req:"yes", value:env.order.detail.applicant.name});
+        if(env.company == "citicpru"){
+            v.push({name:'是否自动垫交保费', code:"autoPayFlag", type:"switch", req:"yes", value: bauto, options:[["Y","是"],["N","否"]]});
+            v.push({name:'自动垫交保费释义', code:"autoPayFlagDesc", type:"label", value: "自动垫交保险费是一项权益。如果选择本权益，续期保费可以用现金价值扣除未还借款本息之后的余额垫缴，同时保险公司会收取一定的利息。"});
+        }
+        return this.buildForm(v);
+    }
 }
+
+class PayRenewForm extends Form {
+    form() {
+        let bc = null;
+        let bk = null;
+        let bcity = null;
+        let bauto = null;
+        if (env.order.extra != null && env.order.extra.payRenew != null) {
+            bc = env.order.extra.payRenew.bankCard;
+            bk = env.order.extra.payRenew.bank;
+            bcity = env.order.extra.payRenew.bankCity;
+            bauto = env.order.extra.payRenew.autoPayFlag;
+        }
+        let v = [
+            {name:'开户银行', code:"bank", type:"select", req:"yes", value: bk, options:env.dict.bank},
+            {name:'银行帐号', code:"bankCard", type:"number", req:"yes", value: bc, mistake:"请输入正确的银行卡号", desc:"银行卡号码"}
+        ];
+        if(env.company == "shlife" || env.company == "citicpru") {
+            v.push({name:'开户行所在地区', code:"bankCity", type:"bankCity", value: bcity, company: env.company, refresh:"yes", req:"yes", desc:"开户行所在地"});
+        }
+        v.push({name:'开户人', code:"bankUser", type:"static", req:"yes", value:env.order.detail.applicant.name});
+		if(env.company == "citicpru"){
+            v.push({name:'是否自动垫交保费', code:"autoPayFlag", type:"switch", req:"yes", value: bauto, options:[["Y","是"],["N","否"]]});
+            v.push({name:'自动垫交保费释义', code:"autoPayFlagDesc", type:"label", value: "自动垫交保险费是一项权益。如果选择本权益，续期保费可以用现金价值扣除未还借款本息之后的余额垫缴，同时保险公司会收取一定的利息。"});
+		}
+        return this.buildForm(v);
+    }
+};
+
+/** 支付选择 **/
+var PaySwich = React.createClass({
+	getInitialState() {
+		return {payOptions: [], hasShow: false};
+	},
+	componentDidMount(){
+	},
+	reSetOptions(options){
+        if(!!options) {
+            this.setState({payOptions: options, hasShow: true});
+        }
+	},
+	toPay(idx){
+		if(this.state.payOptions == null || this.state.payOptions.length <= 0) {
+			return false;
+		}
+		if(this.state.payOptions[idx] == null) {
+			return false;
+		}
+		var vv = this.state.payOptions[idx];
+        let f = common.initForm(vv.nextUrl, vv.params, vv.method);
+        f.submit();
+	},
+    hide(){
+		this.setState({hasShow: false});
+	},
+	render(){
+		let x = 0;
+		return (
+			<div className="notice" style={{display: this.state.hasShow ? "block" : "none"}}>
+				<div className="content">
+					{this.state.payOptions == null || this.state.payOptions.length <= 0 ?
+						<div style={{backgroundColor: "#e93a3a", color: "#FFFFFF", height: "50px", lineHeight: "50px", margin: "15px 20px 10px", borderRadius: "5px"}}>没有支付方式</div> : this.state.payOptions.map(v => {
+                        return (
+							<div style={{backgroundColor: !v.bgColor ? "#1da2e5" : v.bgColor, color: !v.color ? "#FFFFFF" : v.color, height: "50px", lineHeight: "50px", margin: "15px 20px 10px", borderRadius: "5px"}} onClick={this.toPay.bind(this, x++)}>{v.showName}</div>
+                        );
+                    })}
+					{<div style={{backgroundColor: "#bdbdbd", color: "#FFFFFF", height: "50px", lineHeight: "50px", margin: "15px 20px 10px", borderRadius: "5px"}} onClick={this.hide.bind(this)}>取消</div>}
+				</div>
+			</div>
+		);
+	}
+});
 
 var Ground = React.createClass({
 	getInitialState() {
-		return {isSubmit: false, dict:false};
+		return {isSubmit: false, dict:false, disputeShow: false, disputeList: []};
 	},
 	refresh() {
         common.req("dict/view.json", {company: env.company, name: "bank,relation", version: "new"}, r => {
@@ -67,12 +145,17 @@ var Ground = React.createClass({
         }, f => {
             ToastIt("字典数据加载失败");
         });
+        try{this.reloadDisputeData(env.order.detail.applicant.city.code);}catch(e){}
 	},
 	componentDidMount() {
 		this.refresh();
     },
 	submit() {
 		if (this.refs.pay != null && !this.refs.pay.verifyAll()) {
+			ToastIt("请检查支付信息");
+			return;
+		}
+		if (this.refs.payRenew != null && !this.refs.payRenew.verifyAll()) {
 			ToastIt("请检查支付信息");
 			return;
 		}
@@ -85,6 +168,15 @@ var Ground = React.createClass({
             env.order.extra.pay = this.refs.pay.val();
             env.order.extra.pay.bankText = this.refs.pay.refs.bank.text();
             if (env.order.extra.pay.bank == null || env.order.extra.pay.bank == "") {
+            	this.refresh();
+                ToastIt("请选择银行");
+                return;
+			}
+		}
+		if (this.refs.payRenew) {
+            env.order.extra.payRenew = this.refs.payRenew.val();
+            env.order.extra.payRenew.bankText = this.refs.payRenew.refs.bank.text();
+            if (env.order.extra.payRenew.bank == null || env.order.extra.payRenew.bank == "") {
             	this.refresh();
                 ToastIt("请选择银行");
                 return;
@@ -111,12 +203,25 @@ var Ground = React.createClass({
         this.setState({isSubmit: true}, ()=>{
             common.req("sale/apply.json", env.order, r => {
                 common.save("iyb/orderId", env.order.id);
-                if (r.success != false && r.nextUrl != null) {
+                if (r.success != false) {
+                    if(r.nextUrl != null && r.params != null){
+                        var f = common.initForm(r.nextUrl, r.params, r.method);
+                        f.submit();
+                    } else if (r.paySwich != null) {
+                        this.refs.paySwich.reSetOptions(r.paySwich);
+                    } else {
+                        document.location.href = r.nextUrl;
+					}
+                }else{
+                    ToastIt(r.errCode + " - " + r.errMsg);
+                }
+                this.setState({isSubmit: false});
+                /*if (r.success != false && r.nextUrl != null) {
                     document.location.href = r.nextUrl;
                 } else {
                     ToastIt(r.errCode + " - " + r.errMsg);
                     this.setState({isSubmit: false});
-                }
+                }*/
             }, r => {
                 if(r != null){
                     ToastIt(r);
@@ -135,6 +240,9 @@ var Ground = React.createClass({
             if (this.refs.pay) {
                 env.order.extra.pay = this.refs.pay.val();
             }
+            if (this.refs.payRenew) {
+                env.order.extra.payRenew = this.refs.payRenew.val();
+            }
 			if (this.refs.photos) {
 				env.order.extra.photos = this.refs.photos.val();
             }
@@ -143,6 +251,25 @@ var Ground = React.createClass({
 		}
 		document.location.href = link;
 	},
+	// 纠纷相关div数据
+	reloadDisputeData(cityCode){
+        //
+		if(cityCode == null || cityCode.length < 6){
+			return null;
+		}
+        common.req("sale/dispute.json", {provinceId: cityCode.substr(0, 2) + "0000", cityId: cityCode.substr(0, 4) + "00"}, r => {
+        	this.setState({disputeList: r});
+        }, f => {
+            ToastIt("纠纷数据加载失败");
+        });
+	},
+	openDivDispute() {
+
+        $(this.refs.disputeDiv).show();
+	},
+    hide() {
+		$(this.refs.disputeDiv).hide();
+	},
 	render() {
 		let docs = [];
 		if (env.order.detail.read) for (let d in env.order.detail.read) {
@@ -150,6 +277,13 @@ var Ground = React.createClass({
 				docs.push("、");
 			docs.push(<a key={d} onClick={this.openDoc.bind(this, env.order.detail.read[d].url)}>{env.order.detail.read[d].name}</a>);
 		}
+		try{
+            if(env.company == "citicpru" && (""+env.order.detail.applicant.city.code).startsWith("44") && this.state.disputeList != null && this.state.disputeList.length > 0) {	// 440000
+                if (docs.length > 0)
+                    docs.push("、");
+                docs.push(<a onClick={this.openDivDispute.bind(this)}>《广东纠纷调节处信息》</a>);
+            }
+		}catch(e){console.log("添加广东纠纷调节处信息失败");}
 		return (
 			<div className="common">
 				<div className="title">投保信息</div>
@@ -222,6 +356,14 @@ var Ground = React.createClass({
 						<PayForm ref="pay"/>
 					</div>
                 }
+                { !env.order.extra.hasPayRenew ? null :
+                    <div className="title">续期支付信息</div>
+                }
+                { !env.order.extra.hasPayRenew ? null :
+					<div className="form">
+						<PayRenewForm ref="payRenew"/>
+					</div>
+                }
 				{ env.order.detail.photo == null || env.order.detail.photo <= 0 ? null :
 					<div className="title">身份证拍照</div>
 				}
@@ -246,6 +388,40 @@ var Ground = React.createClass({
 						</div>
 					</div>
 				</div>
+				<div ref="disputeDiv" className="notice dispute" style={{display: this.state.disputeShow ? "block" : "none"}} onclick={this}>
+					<div className="content" style={{padding: "20px", textAlign: "left"}}>
+						{
+							this.state.disputeList != null && this.state.disputeList.length > 0 ?
+								this.state.disputeList.map((rdp) => {
+									return <div style={{borderLeft: "3px solid #00fff37a", marginBottom: "5px"}}>
+										<table style={{fontSize: "0.8em"}}>
+											<tr>
+												<td colSpan={2} style={{fontWeight: "bold"}}>{rdp.lnam01}</td>
+											</tr>
+											<tr>
+												<td>地址：</td>
+												<td>{rdp.lnam02}</td>
+											</tr>
+											<tr>
+												<td>电话：</td>
+												<td>{rdp.rmblphone}</td>
+											</tr>
+											<tr>
+												<td>区域：</td>
+												<td>{rdp.sbusiorgid}</td>
+											</tr>
+											<tr>
+												<td>连接：</td>
+												<td>{rdp.email}</td>
+											</tr>
+										</table>
+									</div>;
+								}) : null
+						}
+                        {<div style={{backgroundColor: "#bdbdbd", color: "#FFFFFF", height: "50px", lineHeight: "50px", margin: "15px 20px 0px", borderRadius: "5px", textAlign: "center"}} onClick={this.hide.bind(this)}>已知晓</div>}
+					</div>
+				</div>
+				<PaySwich ref="paySwich"></PaySwich>
 			</div>
 		);
 	}
