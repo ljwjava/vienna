1314df2ab206b11c1e3d82d2d3d652597b17cbc2
lex1982103 <lerrain@gmail.com>
2018-04-12 17:33:35 +0800

    ARC

--- vienna/static/admin/function.jsx
+++ vienna/static/admin/function.jsx
@@ -4,25 +4,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 var ENV = {
-    script: "",
-    reqParams: "",
-    reqSupply: "",
-    gatewayMap: {},
-    envMap: {}
+	target: 1,
+	script: "",
+	reqParams: "",
+	reqSupply: "",
+	envMap: {}
 };
 
 var Main = React.createClass({
 	getInitialState() {
-		return {envList:[], gatewayList:[]};
+		return {envList:[], functionList:[]};
 	},
-	componentWillMount() {
-		common.req("develop/list_gateway.json", {}, r => {
-			var gatewayList = r.map(x => {
-				ENV.gatewayMap[x.id + ""] = x;
-				return <option value={x.id}>{x.uri}</option>;
-			});
-			this.setState({gatewayList:gatewayList});
-		});
+	componentDidMount() {
 		common.req("develop/list_env.json", {}, r => {
 			var envList = [];
 			for (var envId in r) {
@@ -32,7 +25,19 @@ var Main = React.createClass({
 			}
 			this.setState({envList:envList});
 		});
-	},
+
+        var sct = [document.getElementById("script"), document.getElementById("reqParams")];
+        for (var x in sct) sct[x].onkeydown = function(e) {
+            if (e.keyCode === 9){
+                var position = this.selectionStart + 4;
+                this.value = this.value.substr(0, this.selectionStart) + '    ' + this.value.substr(this.selectionStart);
+                this.selectionStart = position;
+                this.selectionEnd = position;
+                this.focus();
+                e.preventDefault();
+            }
+        };
+    },
 	save() {
 		var url = this.getUrl();
 		if (url != null) common.req("develop/save.json", {
@@ -44,13 +49,18 @@ var Main = React.createClass({
 	},
 	apply() {
 		var req = null;
-		if (ENV.gatewayId != null) {
+		if (ENV.funcId != null) {
 			req = {
-				type: 1,
-				gatewayId: ENV.gatewayId,
+				type: 2,
+				envId: ENV.envId,
+				functionId: ENV.funcId,
+				name: this.refs.funcName.value,
+				params: this.refs.funcParams.value,
 				script: this.refs.script.value
-			};
-			ENV.gatewayMap[ENV.gatewayId].script = req.script;
+			}
+			ENV.funcMap[ENV.funcId].name = req.name;
+			ENV.funcMap[ENV.funcId].params = req.params;
+			ENV.funcMap[ENV.funcId].script = req.script;
 		}
 		if (req != null) common.req("develop/apply.json", req, r => {
 			alert("success");
@@ -72,10 +82,12 @@ var Main = React.createClass({
 	},
 	replace() {
 		var req = null;
-		if (ENV.gatewayId != null) {
+		if (ENV.funcId != null) {
 			req = {
-				type: 1,
-				gatewayId: ENV.gatewayId,
+				type: 2,
+				envId: ENV.envId,
+				name: this.refs.funcName.value,
+				params: this.refs.funcParams.value,
 				script: this.refs.script.value
 			}
 		}
@@ -93,17 +105,23 @@ var Main = React.createClass({
 			return null;
 		return reqUrl;
 	},
-	setGateway() {
-		ENV.gatewayId = this.refs.gatewayList.value;
-		if (ENV.gatewayId == "") ENV.gatewayId = null;
-
-		ENV.reqSupply = "";
-		this.refs.reqSupply.value = ENV.reqSupply;
-		this.refs.envList.value = ENV.gatewayMap[ENV.gatewayId].envId;
-
-		var url = this.getUrl();
-		if (url != null) common.req("develop/req_testing.json", {url: url}, r => {
-			ENV.reqParams = r == null ? null : r.param;
+	setEnv() {
+		ENV.funcId = null;
+		ENV.envId = this.refs.envList.value;
+		if (ENV.envId == "") ENV.envId = null;
+		if (ENV.envId != null) common.req("develop/list_function.json", {envId: ENV.envId}, r => {
+			ENV.funcMap = {};
+			var functionList = r.map(x => {
+				ENV.funcMap[x.id + ""] = x;
+				return (<option value={x.id}>{"函数：" + x.name + (x.aka == null ? "" : "，别名：" + x.aka)}</option>);
+			});
+			this.setState({functionList:functionList});
+		});
+	},
+	setFunction() {
+		ENV.funcId = this.refs.funcList.value;
+		if (ENV.funcId == "") ENV.funcId = null;
+		if (ENV.funcId != null) common.req("develop/function.json", {functionId: ENV.funcId}, r => {
 			this.refresh();
 		});
 	},
@@ -120,48 +138,52 @@ var Main = React.createClass({
 		}
 	},
 	refresh() {
+		if (ENV.funcId != null) {
+			this.refs.funcName.value = ENV.funcMap[ENV.funcId].name;
+			this.refs.funcParams.value = ENV.funcMap[ENV.funcId].params;
+		}
 		this.refs.reqParams.value = ENV.reqParams;
-		this.refs.reqSupply.value = ENV.reqSupply;
 
 		ENV.script = "";
-		if (ENV.gatewayId != null)
-			ENV.script = ENV.gatewayMap[ENV.gatewayId].script;
+		if (ENV.funcId != null)
+			ENV.script = ENV.funcMap[ENV.funcId].script;
 		this.refs.script.value = ENV.script;
 	},
 	render() {
 		return (
 			<div className="container-fluid mt-3 mb-3 form-horizontal">
 				<div className="form-row mb-3">
-					<div className="col-sm-6">
-						<select className="form-control" ref="gatewayList" onChange={this.setGateway}><option value="">请选择</option>{this.state.gatewayList}</select>
+					<div className="col-sm-3">
+						<select className="form-control" ref="envList" onChange={this.setEnv}><option value="">请选择</option>{this.state.envList}</select>
 					</div>
 					<div className="col-sm-3">
-						<input className="form-control" ref="reqSupply" placeholder="URL补充"></input>
+						<select className="form-control" ref="funcList" onChange={this.setFunction}><option value="">请选择</option>{this.state.functionList}</select>
 					</div>
 					<div className="col-sm-3">
-						<select className="form-control" ref="envList"><option value=""></option>{this.state.envList}</select>
+						<input className="form-control" ref="funcName" placeholder="函数名"></input>
+					</div>
+					<div className="col-sm-3">
+						<input className="form-control" ref="funcParams" placeholder="参数（多个用逗号隔开）"></input>
 					</div>
 				</div>
 				<div className="form-row">
 					<div className="col-sm-8">
-						<textarea ref="script" className="form-control" style={{height:"600px"}}></textarea>
+						<textarea id="script" ref="script" className="form-control" style={{height:"700px"}}></textarea>
 					</div>
 					<div className="col-sm-4">
-						<textarea ref="reqParams" className="form-control" style={{height:"600px"}}></textarea>
+						<textarea id="reqParams" ref="reqParams" className="form-control" style={{height:"700px"}}></textarea>
 					</div>
 				</div>
 				<div className="form-row mt-3">
 					<div className="col-sm-8">
-						<input type="button" className="btn btn-primary btn-lg" value="请求 >>>>" onClick={this.test}/>
-						&nbsp;&nbsp;
-						<input type="button" className="btn btn-primary btn-lg" value="测试 >>>>" onClick={this.replace}/>
+						<input type="button" className="btn btn-primary btn-lg" value="测试 >>>>" onClick={this.test}/>
 						&nbsp;&nbsp;
 						<input type="button" className="btn btn-primary btn-lg" value="重置 >>>>" onClick={this.reset}/>
 					</div>
 					<div className="col-sm-4" style={{textAlign:"right"}}>
-						<input type="button" className="btn btn-primary btn-lg" value="保存 >>>>" onClick={this.save}/>
+						<input type="button" className="btn btn-primary btn-lg" value="暂存 >>>>" onClick={this.save}/>
 						&nbsp;&nbsp;
-						<input type="button" className="btn btn-primary btn-lg" value="应用 >>>>" onClick={this.apply}/>
+						<input type="button" className="btn btn-primary btn-lg" value="生效 >>>>" onClick={this.apply}/>
 					</div>
 				</div>
 				<div className="form-row">
