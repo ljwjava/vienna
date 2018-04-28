b9e39ed5ac3214b537c49a4b98fdb8cfdf5e40e7
xuechuangwei6 <1399105733@qq.com>
2018-04-28 10:14:38 +0800

    雷达图优化

--- srv-lifeins/src/main/lerrain/service/lifeins/format/FGraphFilter.java
+++ srv-lifeins/src/main/lerrain/service/lifeins/format/FGraphFilter.java
@@ -25,26 +25,28 @@ public class FGraphFilter implements FilterPlan
 	{
 		list = new ArrayList<>();
 		list.add(new Category("医疗保障", "COMMON_MEDICAL + CANCER_MEDICAL", new int[] {0,10000,50000,100000,1000000},
-			new Item("一般医疗保险金", "'因意外伤害或非等待期因患疾病，在二级及以上医院普通部发生需个人支付，必须且合理的住院或特殊门诊费用最高可报销'+format('%.2f', COMMON_MEDICAL/10000)+'万。'", "COMMON_MEDICAL > 0"),
-			new Item("癌症医疗保险金", "'罹患癌症，在二级及以上医院普通部发生需个人支付，必须且合理的住院或特殊门诊费用最高可报销'+format('%.2f', CANCER_MEDICAL/10000)+'万。无免赔。'", "COMMON_MEDICAL > 0")
+			new Item("一般医疗保险金", "'因意外伤害或非等待期因患疾病，在二级及以上医院普通部发生需个人支付，必须且合理的住院或特殊门诊费用最高可报销'+format('%.2f', COMMON_MEDICAL/10000)+'万。'", "COMMON_MEDICAL != null && COMMON_MEDICAL > 0"),
+			new Item("癌症医疗保险金", "'罹患癌症，在二级及以上医院普通部发生需个人支付，必须且合理的住院或特殊门诊费用最高可报销'+format('%.2f', CANCER_MEDICAL/10000)+'万。无免赔。'", "COMMON_MEDICAL != null && COMMON_MEDICAL > 0")
 		));
-		list.add(new Category("伤残保障", "max(ACCIDENT_DISABILITY, OTHER_DISABILITY, TOTAL_DISABILITY[1])", new int[] {0,100000,500000,1000000,2000000},
-			new Item("全残保障", "'若被保人不幸全残，给付全残保险金，最高首年'+format('%.2f', TOTAL_DISABILITY[0]/10000)+'万，次年及以后'+format('%.2f', TOTAL_DISABILITY[1]/10000)+'万。'", "TOTAL_DISABILITY != null && TOTAL_DISABILITY[0] > 0")
+		list.add(new Category("伤残保障", "max(ACCIDENT_DISABILITY, OTHER_DISABILITY, try(TOTAL_DISABILITY[1], 0))", new int[] {0,100000,500000,1000000,2000000},
+			new Item("全残保障", "'若被保人不幸全残，给付全残保险金，最高首年'+format('%.2f', try(TOTAL_DISABILITY[0]/10000,0))+'万，次年及以后'+format('%.2f', try(TOTAL_DISABILITY[1]/10000,0))+'万。'", "try(TOTAL_DISABILITY[0],0) > 0"),
+			new Item("意外伤残", "'若被保人因意外伤害造成伤残，按照伤残评定标准对应等级的给付比例乘以伤残保额，将给付伤残保险金，最高为'+format('%.2f', OTHER_DISABILITY/10000)+'万。'", "OTHER_DISABILITY != null && OTHER_DISABILITY > 0")
+
 		));
 		list.add(new Category("重疾保障", "max(THUNDER[0], THUNDER[1])", new int[] {0,100000,200000,500000,1000000},
 			new Item(null, "'罹患所保障的重大疾病，保险公司给付重疾保险金，最高首年'+format('%.2f', THUNDER[0]/10000)+'万，次年及以后'+format('%.2f', THUNDER[1]/10000)+'万。'", "THUNDER != null && THUNDER[0] > 0"),
-			new Item("投保人重疾豁免", "'投保人罹患所重疾，免缴部分剩余保费'", "A_THUNDER_EXEMPT > 0")
+			new Item("投保人重疾豁免", "'投保人罹患所重疾，免缴部分剩余保费'", "A_THUNDER_EXEMPT != null && A_THUNDER_EXEMPT > 0")
 		));
 		list.add(new Category("轻症保障", "MILD", new int[] {0,20000,50000,100000,200000},
-			new Item(null, "'罹患所保障的轻症疾病，保险公司给付轻症保险金最高'+format('%.2f', MILD/10000)+'万。'", "MILD > 0"),
-			new Item("被保险人轻症豁免", "'被保险人罹患所保障的轻症疾病，免缴部分剩余保费'", "MILD_EXEMPT > 0"),
-			new Item("投保人轻症豁免", "'投保人罹患所保障的轻症疾病，免缴部分剩余保费'", "A_MILD_EXEMPT > 0")
+			new Item(null, "'罹患所保障的轻症疾病，保险公司给付轻症保险金最高'+format('%.2f', MILD/10000)+'万。'", "MILD!= null && MILD > 0"),
+			new Item("被保险人轻症豁免", "'被保险人罹患所保障的轻症疾病，免缴部分剩余保费'", "MILD_EXEMPT !=null && MILD_EXEMPT > 0"),
+			new Item("投保人轻症豁免", "'投保人罹患所保障的轻症疾病，免缴部分剩余保费'", "A_MILD_EXEMPT !=null && A_MILD_EXEMPT > 0")
 		));
 		list.add(new Category("身故保障", "max(ACCIDENT_DEATH, OTHER_DEATH)", new int[] {0,100000,500000,1000000,2000000},
-			new Item("意外身故", "'被保人因意外伤害原因导致身故，保险公司给付将身故保险金，最高为'+format('%.2f', ACCIDENT_DEATH/10000)+'万。'", "ACCIDENT_DEATH > 0"),
-			new Item("非意外身故", "'若被保人因非意外伤害原因导致身故，保险公司给付身故保险金，最高为'+format('%.2f', OTHER_DEATH/10000)+'万。'", "OTHER_DEATH > 0"),
-			new Item("身故返还", "'被保人身故，保险公司给付身故保险金，为和谐健康之享主险已交保费'", "PAYBACK_DEATH > 0"),
-			new Item("身故豁免", "'若投保人因意外或等待期（180天）后因非意外的原因导致身故，免缴部分剩余保费。'", "A_DEATH_EXEMPT > 0")
+			new Item("意外身故", "'被保人因意外伤害原因导致身故，保险公司给付将身故保险金，最高为'+format('%.2f', ACCIDENT_DEATH/10000)+'万。'", "ACCIDENT_DEATH != null && ACCIDENT_DEATH > 0"),
+			new Item("非意外身故", "'若被保人因非意外伤害原因导致身故，保险公司给付身故保险金，最高为'+format('%.2f', OTHER_DEATH/10000)+'万。'", "OTHER_DEATH != null && OTHER_DEATH > 0"),
+			new Item("身故返还", "'被保人身故，保险公司给付身故保险金，为和谐健康之享主险已交保费'", "PAYBACK_DEATH != null && PAYBACK_DEATH > 0"),
+			new Item("身故豁免", "'若投保人因意外或等待期（180天）后因非意外的原因导致身故，免缴部分剩余保费。'", "A_DEATH_EXEMPT != null && A_DEATH_EXEMPT > 0")
 		));
 	}
 
