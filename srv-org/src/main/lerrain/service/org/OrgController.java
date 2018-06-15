cd4de5c43907f7346cff2a8370ef8b7230522b85
iyb-wangguangrong <wangguangrong@iyunbao.com>
2018-06-15 16:51:40 +0800

    云服机构

--- srv-org/src/main/lerrain/service/org/OrgController.java
+++ srv-org/src/main/lerrain/service/org/OrgController.java
@@ -1,18 +1,27 @@
 package lerrain.service.org;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
 @Controller
 public class OrgController
 {
     @Autowired
     OrgService orgSrv;
+    @Autowired
+    MemberService memberSrv;
+    @Autowired
+    EnterpriseService enterpriseSrv;
 
     @RequestMapping("/member.json")
     @ResponseBody
@@ -20,10 +29,11 @@ public class OrgController
     {
         Long memberId = json.getLong("memberId");
         Long userId = json.getLong("userId");
+        int userType = json.getIntValue("userType");
 
         JSONObject res = new JSONObject();
         res.put("result", "success");
-        res.put("content", memberId != null ? orgSrv.getMember(memberId) : orgSrv.findMember(userId));
+        res.put("content", memberId != null ? orgSrv.getMember(memberId) : orgSrv.findMember(userId, userType));
 
         return res;
     }
@@ -78,5 +88,150 @@ public class OrgController
         res.put("content", r);
 
         return res;
+    }
+
+    @RequestMapping("/saveEnterpriseInfo.json")
+    @ResponseBody
+    public JSONObject saveEnterpriseInfo(@RequestBody JSONObject json) {
+        JSONObject result = new JSONObject();
+        Long id = -1l;
+        int type = json.getIntValue("type");
+        String mobile = json.getString("mobile");
+        String name = json.getString("name");
+        // 邀请码
+        Long inviteCode = json.getLong("inviteCode");
+        if (1 == type) {
+            // 保存渠道/子账户信息
+            Member member = new Member();
+            member.setName(name);
+            member.setMobile(mobile);
+            Long orgId = json.getLong("orgId");
+            Long companyId = json.getLong("companyId");
+            member.setOrgId(orgId);
+            member.setCompanyId(companyId);
+            member.setStatus(1);
+            id = memberSrv.save(member);
+        } else if (2 == type) {
+            // 保存渠道/部门信息
+            Org org = new Org();
+            org.setName(name);
+            org.setMobile(mobile);
+            org.setType(4);
+            Enterprise enterprise = new Enterprise();
+            enterprise.setCompanyName(name);
+            enterprise.setTelephone(mobile);
+            enterprise.setParentId(inviteCode);
+            Long companyId = enterpriseSrv.save(enterprise);
+            org.setCompanyId(companyId);
+            org.setType(4);
+            id = orgSrv.save(org);
+        } else if (3 == type) { // 保存机构信息
+            // 保存渠道/部门信息
+            Org org = new Org();
+            org.setName(name);
+            org.setMobile(mobile);
+            org.setType(4);
+            Long companyId = json.getLong("companyId");
+            Long parentOrgId = json.getLong("parentOrgId");
+            org.setParentId(parentOrgId);
+            org.setCompanyId(companyId);
+            org.setType(4);
+            id = orgSrv.save(org);
+        }
+        result.put("result", "success");
+        result.put("content", id);
+        return result;
+    }
+
+    @RequestMapping("/updMember.json")
+    @ResponseBody
+    public JSONObject updMember(@RequestBody JSONObject json) {
+        JSONObject result = new JSONObject();
+        Member member = JSON.parseObject(json.toJSONString(), Member.class);
+        boolean flag = memberSrv.update(member);
+        result.put("result", "success");
+        result.put("content", flag);
+
+        return result;
+    }
+
+    @RequestMapping("/updOrg.json")
+    @ResponseBody
+    public JSONObject updOrg(@RequestBody JSONObject json) {
+        JSONObject result = new JSONObject();
+        Org org = JSON.parseObject(json.toJSONString(), Org.class);
+        boolean flag = orgSrv.update(org);
+        result.put("result", "success");
+        result.put("content", flag);
+
+        return result;
+    }
+
+    @RequestMapping("/removeOrg.json")
+    @ResponseBody
+    public JSONObject removeOrg(@RequestBody JSONObject json) {
+        JSONObject result = new JSONObject();
+        Long id = json.getLong("id");
+        boolean flag = orgSrv.remove(id);
+        result.put("result", "success");
+        result.put("content", flag);
+
+        return result;
+    }
+
+    @RequestMapping("/childOrg.json")
+    @ResponseBody
+    public JSONObject childOrg(@RequestBody JSONObject json) {
+        JSONObject result = new JSONObject();
+        List<Org> org = orgSrv.childOrg(json.getLongValue("orgId"));
+        result.put("result", "success");
+        result.put("content", org);
+
+        return result;
+    }
+
+    @RequestMapping("/memberCountByOrgId.json")
+    @ResponseBody
+    public JSONObject countByOrgId(@RequestBody JSONObject json) {
+        JSONObject result = new JSONObject();
+        Long count = memberSrv.countByOrgId(json.getLongValue("orgId"));
+        result.put("result", "success");
+        result.put("content", count);
+        return result;
+    }
+
+    @RequestMapping("/memberListByOrgId.json")
+    @ResponseBody
+    public JSONObject listByOrgId(@RequestBody JSONObject json) {
+        JSONObject result = new JSONObject();
+        List<Map<String, Object>> list = memberSrv.listByOrgId(json.getLongValue("orgId"), json.getInteger("from"),
+                json.getInteger("number"), json.getString("searchCondition"));
+        result.put("result", "success");
+        result.put("content", list);
+        return result;
+    }
+
+    @RequestMapping("/orgTree.json")
+    @ResponseBody
+    public JSONObject orgTree(@RequestBody JSONObject json) {
+        JSONObject result = new JSONObject();
+        JSONArray orgTreeJson = orgSrv.orgTree(json.getLongValue("orgId"));
+        result.put("result", "success");
+        result.put("content", orgTreeJson);
+
+        return result;
+    }
+
+    @RequestMapping("/companySubordinate.json")
+    @ResponseBody
+    public JSONObject companySubordinate(@RequestBody JSONObject json) {
+        JSONObject result = new JSONObject();
+        List<Enterprise> list = enterpriseSrv.querySubordinate(json.getLongValue("companyId"));
+        result.put("result", "success");
+        JSONObject rJson = new JSONObject();
+        rJson.put("list", list.size());
+        rJson.put("list", list);
+        result.put("content", rJson);
+        return result;
     }
 }
--- srv-org/src/main/lerrain/service/org/OrgService.java
+++ srv-org/src/main/lerrain/service/org/OrgService.java
@@ -1,11 +1,17 @@
 package lerrain.service.org;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import lerrain.service.common.Log;
+import lerrain.service.common.ServiceTools;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 
 @Service
 public class OrgService
@@ -18,6 +24,9 @@ public class OrgService
 
 	@Autowired
 	CompanyDao companyDao;
+
+    @Autowired
+    ServiceTools       tools;
 
 	Map<Long, Member> m1 = new HashMap<>();
 	Map<Long, Org> m2 = new HashMap<>();
@@ -104,4 +113,80 @@ public class OrgService
 			return null;
 		}
 	}
+
+    public Object findMember(Long userId, int userType) {
+        try {
+            // 判断用户是部门还是子账户
+            if (2 == userType) {
+                return orgDao.loadOrg(userId);
+            } else {
+                return memberDao.findMember(userId);
+            }
+        } catch (Exception e) {
+            Log.info("not found - " + userId);
+            return null;
+        }
+    }
+
+    public Long save(Org org) {
+        // 主键ID与userId保持一致
+        Long id = tools.nextId("user");
+        org.setId(id);
+        int result = orgDao.save(org);
+        if (result > 0) {
+            return id;
+        }
+        return null;
+    }
+
+    public boolean update(Org org) {
+        return orgDao.update(org);
+    }
+
+    public boolean remove(Long id) {
+        return orgDao.remove(id);
+    }
+
+    public List<Org> childOrg(Long id) {
+        return orgDao.childOrg(id);
+    }
+
+    public JSONArray orgTree(Long id) {
+        JSONArray array = new JSONArray();
+        JSONObject result = new JSONObject();
+        array.add(result);
+        Org currentOrg = orgDao.loadOrg(id);
+        result.put("key", currentOrg.getId());
+        result.put("title", currentOrg.getName());
+        // 获取子机构
+        List<Org> list = orgDao.querySubordinateById(currentOrg.getId());
+        JSONArray listArray = new JSONArray();
+        if (list != null && list.size() > 0) {
+            for (Org org : list) {
+                listArray.add(parseOrgTree(org));
+            }
+        }
+        if (listArray != null && listArray.size() > 0) {
+            result.put("children", listArray);
+        }
+        return array;
+    }
+
+    private JSONObject parseOrgTree(Org org) {
+        JSONObject result = new JSONObject();
+        result.put("key", org.getId());
+        result.put("title", org.getName());
+        List<Org> list = orgDao.querySubordinateById(org.getId());
+        JSONArray listArray = new JSONArray();
+        if (list != null && list.size() > 0) {
+            for (Org orgs : list) {
+                listArray.add(parseOrgTree(orgs));
+            }
+        }
+        if (listArray != null && listArray.size() > 0) {
+            result.put("children", listArray);
+        }
+        return result;
+    }
+
 }
